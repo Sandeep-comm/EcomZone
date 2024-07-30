9f8952a96eeaa6db6bb31664de8ef22cd6b11510
Sandeep <sandeepandru431@gmail.com>
2024-07-30 13:43:46 +0530

    Initial Commit

---
+++ src/main/java/com/ecomzone/ecomzone/EcomzoneApplication.java
@@ -0,0 +1,13 @@
+package com.ecomzone.ecomzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomzoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomzone/ecomzone/EcomzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomzone.ecomzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
